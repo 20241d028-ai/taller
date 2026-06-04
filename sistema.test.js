c457dfe5f0d82ee96e0723bbc634046d517ecddf
CristhianQuispe44 <cristhianquispechallco44@gmail.com>
2026-06-03 19:42:44 -0500

    actual pipiline

--- sistema.test.js
+++ sistema.test.js
@@ -1,7 +1,6 @@
-const {validarTexto}=require('./sistema');
-test('validar que detecte contenido',() =>{
-    const resultar=validarTexto("hola");
+const { validarTexto } = require('./sistema');
+
+test('validar que detecte contenido', () => {
+    const resultado = validarTexto("hola");
     expect(resultado).toBe("con-texto");
-
 });
-
